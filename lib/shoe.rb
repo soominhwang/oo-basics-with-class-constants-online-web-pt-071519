56c6b7b45ea100ca59527c58ae2db52ef857d8a8
Soomin Hwang <soomin.h.hwang@gmail.com>
2019-08-18 22:32:21 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -6,6 +6,11 @@ class Shoe
     @brand = brand
   end
 
+  def brand=(brand)
+    @brand = brand
+    BRANDS << brand
+  end
+
   def cobble
     self.condition = "new"
     puts "Your shoe is as good as new!"
